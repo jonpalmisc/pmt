025d05cce8e49ba5ce75071d496156d61f30e87d
Jon Palmisciano <jp@jonpalmisc.com>
2020-11-25 13:40:51 -0500

    feat: added basic Mermaid.js plugin

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,5 +1,6 @@
 const markdown = require("./markdown");
+const mermaid = require("./mermaid");
 const mathjax = require("./mathjax");
 const scss = require("./scss");
 
-module.exports = [markdown, mathjax, scss];
+module.exports = [markdown, mathjax, mermaid, scss];
---
+++ src/plugins/mermaid.js
@@ -0,0 +1,36 @@
+const { JSDOM } = require("jsdom");
+const pug = require("pug");
+
+const filter = (text) => {
+  return pug.render("div.mermaid #{text}", { text });
+};
+
+const transformer = async (html) => {
+  const dom = new JSDOM(html);
+
+  // Create the Mermaid.js dependency script.
+  const mermaidScript = dom.window.document.createElement("script");
+  mermaidScript.setAttribute(
+    "src",
+    "https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"
+  );
+
+  // Create another script to initialize Mermaid.js with custom parameters. The
+  // parameters below fix flowcharts in Prince!
+  const initScript = dom.window.document.createElement("script");
+  initScript.text = "mermaid.initialize({ flowchart: { htmlLabels: false } });";
+
+  // Add the scripts to the DOM.
+  dom.window.document.body.appendChild(mermaidScript);
+  dom.window.document.body.appendChild(initScript);
+
+  return dom.serialize();
+};
+
+module.exports = {
+  dependencies: null,
+  filter,
+  filterName: "mermaid",
+  name: "Mermaid",
+  transformer: transformer,
+};
