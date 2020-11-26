e543ec1ac977a08b41cc4c904772969b1779e564
Jon Palmisciano <jp@jonpalmisc.com>
2020-11-25 22:39:25 -0500

    refactor: revamped MathJax plugin

--- src/plugins/mathjax.js
+++ src/plugins/mathjax.js
@@ -1,21 +1,30 @@
-const mathjaxPage = require("mathjax-node-page").mjpage;
+const mathjax = require("mathjax-full");
 
 const transformer = async (html) => {
-  return new Promise((resolve) => {
-    mathjaxPage(
-      html,
-      {
-        format: ["TeX"],
-        singleDollars: true,
-      },
-      {
-        mml: true,
-        css: true,
-        html: true,
-      },
-      (output) => resolve(output)
-    );
+  const mj = await mathjax.init({
+    loader: {
+      load: ["adaptors/liteDOM", "tex-chtml"],
+    },
+    tex: {
+      packages: ["base", "autoload", "require", "ams", "newcommand"],
+    },
+    "adaptors/liteDOM": {
+      fontSize: 16,
+    },
+    chtml: {
+      fontURL:
+        "https://cdn.jsdelivr.net/npm/mathjax@3/es5/output/chtml/fonts/woff-v2",
+      exFactor: 0.5,
+    },
+    startup: {
+      document: html,
+    },
   });
+
+  const adaptor = mj.startup.adaptor;
+  const output = mj.startup.document;
+
+  return adaptor.outerHTML(adaptor.root(output.document));
 };
 
 module.exports = {
