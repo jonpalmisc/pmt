b644cf2e29ec181d19aa556dcc8f8f1f56960070
Jon Palmisciano <jp@jonpalmisc.com>
2020-11-26 08:08:09 -0500

    fix: added ESM so MathJax works w/o extra flags

---
+++ src/main.js
@@ -0,0 +1,154 @@
+const cp = require("child_process");
+const fs = require("fs");
+
+const engine = require("./engine");
+const debug = require("./debug");
+const misc = require("./misc");
+
+const puppeteer = require("puppeteer");
+
+const puppeteerConfig = {
+  headless: true,
+  args: ["--disable-translate", "--disable-extensions", "--disable-sync"],
+};
+
+async function main(args) {
+  global.pdtDebug = args.debug;
+
+  debug("PDT initialized.");
+
+  debug("Checking if input file exists...");
+  if (!fs.existsSync(args.input)) {
+    console.log(`Error: Couldn't find input file "${args.input}".`);
+    return;
+  }
+
+  // Attempt to dender the input to HTML; show errors and stop if needed.
+  let staticHtml;
+  try {
+    staticHtml = await engine.renderFile(args.input, {
+      plugins: args.plugins,
+    });
+  } catch (error) {
+    // Handle Pug-related errors a little more gracefully.
+    if (error.code && error.code.startsWith("PUG")) {
+      console.error(
+        `Error: Failed to compile Pug. (${error.msg}, ${error.filename}:${error.line})`
+      );
+
+      // Show hint for missing filter errors.
+      if (error.code == "PUG:UNKNOWN_FILTER") {
+        console.error("Hint: Did you forget to enable a plugin?");
+      }
+    } else {
+      console.error("Error: " + error.message);
+    }
+
+    return;
+  }
+
+  // Determine the correct output path depending on whether it was explicitly
+  // provided and what type of output we are producing.
+  let outputPath = args.output
+    ? args.output
+    : misc.replaceExt(args.input, args.html ? ".html" : ".pdf");
+
+  // If the user wants static output, write it now and exit.
+  if (args.static) {
+    fs.writeFileSync(outputPath, staticHtml);
+    return;
+  }
+
+  // Get a temporary file path and write our static HTML to it.
+  let tempPath = misc.getTempPath(misc.replaceExt(args.input, ".html"));
+
+  debug(`Writing compiled HTML to disk temporarily... (${tempPath})`);
+  fs.writeFileSync(tempPath, staticHtml);
+
+  // Start a headless browser and create a new page.
+  debug("Initializing headless browser...");
+  const browser = await puppeteer.launch(puppeteerConfig);
+  const page = await browser.newPage();
+
+  // Attempt to load the static HTML into the page and hydrate it.
+  try {
+    await engine.hydrateFile(page, tempPath);
+  } catch (error) {
+    console.error("Error: " + error.message);
+    return;
+  }
+
+  if (args.html) {
+    const finalHtml = await page.content();
+
+    debug("Closing browser and saving hydrated HTML...");
+    fs.writeFileSync(outputPath, finalHtml);
+    browser.close();
+    return;
+  }
+
+  try {
+    await engine.makePdf(page, outputPath, "internal");
+  } catch (error) {
+    console.error("Error: " + error.message);
+  }
+
+  // Remove temporary HTML and close the browser.
+  debug(`Removing temporary HTML file... (${tempPath})`);
+  fs.unlinkSync(tempPath);
+  browser.close();
+
+  debug("Done!");
+}
+
+const mainCommand = (yargs) => {
+  yargs
+    .alias("h", "help")
+    .alias("v", "version")
+    .positional("input", {
+      describe: "The file to process",
+      type: "string",
+      default: "main.pug",
+    })
+    .option("debug", {
+      alias: "d",
+      desc: "Show debug and performance info",
+      type: "boolean",
+    })
+    .option("output", {
+      alias: "o",
+      desc: "Write output to a specific path",
+      type: "string",
+    })
+    .option("html", {
+      alias: "x",
+      desc: "Produce HTML for use with another program",
+      type: "boolean",
+    })
+    .option("plugins", {
+      alias: "p",
+      desc: "Enable the specified plugins",
+      type: "array",
+      default: [],
+    })
+    .option("static", {
+      alias: "S",
+      desc: "Skip page hydration",
+      type: "boolean",
+      implies: "html",
+    });
+};
+
+require("yargs")
+  .parserConfiguration({
+    "short-option-groups": true,
+  })
+  .version()
+  .command(
+    "$0 [input] [options]",
+    "Create documents (and more) using Pug.",
+    mainCommand,
+    async (args) => {
+      await main(args);
+    }
+  ).argv;
